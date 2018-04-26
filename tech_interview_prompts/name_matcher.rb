50dd184ca94e1f48ee83989ffa846e282d49724f
Clay Shaw <Claydshaw@gmail.com>
2018-04-26 12:01:08 -0700

    complete logic

--- tech_interview_prompts/name_matcher.rb
+++ tech_interview_prompts/name_matcher.rb
@@ -15,14 +15,14 @@ def name_matcher(arr, name)
       m_initial = m_name[0]
       l_name = poss_name[2]
 
-      middle_names[m_initial] = true
+      # middle_names[m_initial] = true
       middle_names[m_name] = true
 
       first_and_last[f_name] = true
       first_and_last[l_name] = true
     else
-      poss_name.each do |name|
-        first_and_last[name] = true
+      poss_name.each do |n|
+        first_and_last[n] = true
       end
     end
   end
@@ -33,12 +33,14 @@ def name_matcher(arr, name)
     # p first_and_last
     # p names
 
+    if middle_names.keys.none? { |m_name| m_name.index(names[1]) || names[1].index(m_name) } &&
+       first_and_last.keys.none? { |m_name| m_name.index(names[1])  || names[1].index(m_name) } &&
+       !middle_names.empty?
+      # p names[1]
+      return false 
+    end
 
-    return false if !middle_names[names[1]] && 
-                    !middle_names[names[1][0]] &&
-                     middle_names.length > 0
-
-    count += 1 if first_and_last[names[0]]
+    count += 1 if first_and_last[names[0]] || middle_names[names[0]]
     count += 1 if first_and_last[names[2]]
   else
     count += 1 if first_and_last[names[0]]
@@ -47,6 +49,9 @@ def name_matcher(arr, name)
 
   count >= 2
 end
+# we are given a full middle name but none exist in db (more logic to see if db is empty?)
+# 
+
 
 known_aliases = ["Alphonse Gabriel Capone", "Al Capone"]
 p name_matcher(known_aliases, "Alphonse Gabriel Capone") == true
@@ -54,8 +59,8 @@ p name_matcher(known_aliases, "Al Capone") == true
 p name_matcher(known_aliases, "Alphonse Francis Capone") == false
 
 known_aliases = ['Alphonse Capone'];
-p name_matcher(known_aliases, 'Alphonse Gabriel Capone') === true
-p name_matcher(known_aliases, 'Alphonse Francis Capone') === true
+p name_matcher(known_aliases, 'Alphonse Gabriel Capone') === true #
+p name_matcher(known_aliases, 'Alphonse Francis Capone') === true #
 p name_matcher(known_aliases, 'Alexander Capone') === false
 
 known_aliases = ['Alphonse Gabriel Capone']
@@ -78,7 +83,7 @@ p name_matcher(known_aliases, 'Alphonse Gregory Capone') == false
 known_aliases = ['Alphonse Gabriel Capone']
 p name_matcher(known_aliases, 'Gabriel Alphonse Capone') == true
 p name_matcher(known_aliases, 'Gabriel A Capone') == true
-p name_matcher(known_aliases, 'Gabriel Al Capone') == true
+p name_matcher(known_aliases, 'Gabriel Al Capone') == true #
 p name_matcher(known_aliases, 'Gabriel Francis Capone') == false
 
 known_aliases = ['Alphonse Gabriel Capone']
